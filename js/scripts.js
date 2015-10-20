8339ae31aad3c4edbd146a4a50945c7e2b9144b2
Vincent Cerone <vinny.cerone@gmail.com>
2015-10-19 20:59:20 -0400

    initial file upload

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(function() {
+    // Navigation Toggle for Tablet/Mobile
+    $( ".navbar-toggle" ).click(function() {
+        $( ".navmenu" ).toggleClass( "hide" );
+        $(".menu-text").toggleClass("hide");
+    });
+    // End Navigation Toggle
+    $('#datePicker').datepicker({
+
+    });
+
+});/**
+ * Created by Vinny on 9/30/2015.
+ */
